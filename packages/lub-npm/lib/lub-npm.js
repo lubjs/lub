8f8e94bc12ac8cb822ecf18ecd40e77cb37f08d9
ahungrynoob <dxd_sjtu@outlook.com>
2019-11-28 19:53:57 +0800

    feat: finish install and installSync

--- packages/lub-npm/lib/lub-npm.js
+++ packages/lub-npm/lib/lub-npm.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const fs = require('lub-fs');
+const { helper } = require('lub-command');
+const log = require('lub-log')('lub-npm');
+const spawn = require('cross-spawn');
 const path = require('path');
 const u = require('universalify').fromCallback;
 
@@ -68,3 +71,186 @@ function findPackageJson(startDir, callback) {
 }
 
 exports.findPackageJson = u(findPackageJson);
+
+const defaultInstallOption = {
+  save: true,
+  dev: false,
+  global: false,
+  production: false,
+  registry: 'https://registry.npmjs.org',
+  npmClient: 'npm',
+};
+/**
+ * install packages or dependencies from pkg.json
+ * @param {string | array} [pkgs] - packages to install
+ * @param {Object} [option] - option for install
+ * @param {boolean} option.save  - save to pkg[dependencies]
+ * @param {boolean} option.dev  - save to pkg[devDependencies]
+ * @param {boolean} option.global - install to global scope (npm install xx -g)
+ * @param {boolean} option.production - install all dependencies except for pkg[devDependencies]
+ * @param {string} option.registry - set npm registry
+ * @param {string} option.npmClient - support npm | yarn | or your custom npm client like cnpm, tnpm
+ */
+exports.installSync = function(pkgs, option) {
+  function spawnNpm(npmClient, args) {
+    const npmProcess = spawn.sync(npmClient, args, { stdio: 'inherit' });
+    const error = npmProcess.error;
+    if (error && error.code === 'ENOENT') {
+      log.error(
+        `Could not execute ${npmClient}, please check your package manager.`
+      );
+      throw error;
+    }
+  }
+
+  if (pkgs && !Array.isArray(pkgs) && typeof pkgs !== 'string') {
+    // only pass option in the first place;
+
+    // set yarn registry default to 'https://registry.yarnpkg.com'
+    if (pkgs.npmClient === 'yarn' && !pkgs.registry) {
+      pkgs.registry = 'https://registry.yarnpkg.com';
+    }
+    option = Object.assign({}, defaultInstallOption, pkgs);
+    pkgs = [];
+  } else {
+    pkgs = Array.isArray(pkgs) ? pkgs : [ pkgs ];
+    if (option.npmClient === 'yarn' && !option.registry) {
+      option.registry = 'https://registry.yarnpkg.com';
+    }
+    option = Object.assign({}, defaultInstallOption, option);
+  }
+
+  const { npmClient, registry, production, global, save, dev } = option;
+  let args = [];
+  if (!pkgs.length) {
+    // npm install or yarn install
+    args = [ 'install' ];
+    if (production) {
+      args.push('--production');
+    }
+    if (registry) {
+      args.push(`--registry=${args.registry}`);
+    }
+  } else {
+    // npm install package or yarn add
+    if (npmClient === 'yarn' && global) {
+      args.push('global');
+    }
+
+    if (npmClient === 'yarn') {
+      args.push('add');
+    } else {
+      args.push('install');
+    }
+
+    args = args.concat(pkgs);
+
+    if (save && npmClient !== 'yarn' && !dev && !global) {
+      // yarn always add dep to pkg
+      // https://github.com/yarnpkg/yarn/issues/1743#issuecomment-259428370
+      args.push('--save');
+    }
+
+    if (dev && !global) {
+      args.push('-D');
+    }
+
+    if (global && npmClient !== 'yarn') {
+      args.push('-g');
+    }
+  }
+
+  spawnNpm(npmClient, args);
+};
+
+/**
+ * install packages or dependencies from pkg.json
+ * @param {string | array} [pkgs] - packages to install
+ * @param {Object} [option] - option for install
+ * @param {boolean} option.save  - save to pkg[dependencies]
+ * @param {boolean} option.dev  - save to pkg[devDependencies]
+ * @param {boolean} option.global - install to global scope (npm install xx -g)
+ * @param {boolean} option.production - install all dependencies except for pkg[devDependencies]
+ * @param {string} option.registry - set npm registry
+ * @param {string} option.npmClient - support npm | yarn | or your custom npm client like cnpm, tnpm
+ * @param {function} callback - callback with error argv
+ */
+exports.install = u(function(pkgs, option, callback) {
+  function spawnNpm(npmClient, args) {
+    helper.spawn(npmClient, args, { stdio: 'inherit' }).catch(error => {
+      if (error && error.code === 'ENOENT') {
+        log.error(
+          `Could not execute ${npmClient}, please check your package manager.`
+        );
+      }
+      callback(error);
+    });
+  }
+
+  // work around for universalify.fromCallback
+  // https://github.com/RyanZim/universalify/issues/10
+  if (typeof pkgs === 'function') {
+    callback = pkgs;
+    pkgs = [];
+    option = {};
+  }
+
+  if (pkgs && !Array.isArray(pkgs) && typeof pkgs !== 'string') {
+    // only pass option in the first place;
+
+    // set yarn registry default to 'https://registry.yarnpkg.com'
+    if (pkgs.npmClient === 'yarn' && !pkgs.registry) {
+      pkgs.registry = 'https://registry.yarnpkg.com';
+    }
+    option = Object.assign({}, defaultInstallOption, pkgs);
+    pkgs = [];
+  } else {
+    pkgs = Array.isArray(pkgs) ? pkgs : [ pkgs ];
+    if (option.npmClient === 'yarn' && !option.registry) {
+      option.registry = 'https://registry.yarnpkg.com';
+    }
+    option = Object.assign({}, defaultInstallOption, option);
+  }
+
+  const { npmClient, registry, production, global, save, dev } = option;
+  let args = [];
+  if (!pkgs.length) {
+    // npm install or yarn install
+    args = [ 'install' ];
+    if (production) {
+      args.push('--production');
+    }
+    if (registry) {
+      args.push(`--registry=${args.registry}`);
+    }
+  } else {
+    // npm install package or yarn add
+    if (npmClient === 'yarn' && global) {
+      args.push('global');
+    }
+
+    if (npmClient === 'yarn') {
+      args.push('add');
+    } else {
+      args.push('install');
+    }
+
+    args = args.concat(pkgs);
+
+    if (save && npmClient !== 'yarn' && !dev && !global) {
+      // yarn always add dep to pkg
+      // https://github.com/yarnpkg/yarn/issues/1743#issuecomment-259428370
+      args.push('--save');
+    }
+
+    if (dev && !global) {
+      args.push('-D');
+    }
+
+    if (global && npmClient !== 'yarn') {
+      args.push('-g');
+    }
+  }
+
+  spawnNpm(npmClient, args);
+});
